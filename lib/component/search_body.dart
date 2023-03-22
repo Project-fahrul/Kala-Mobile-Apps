616e23e508ef936c7ae67c31bed48878681fde1a
fahrulputra40 <fahrulputra40@gmail.com>
2023-03-22 22:40:38 +0700

    init project

---
+++ lib/component/search_body.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class SearchBody extends StatefulWidget {
+  const SearchBody({super.key});
+
+  @override
+  State<SearchBody> createState() => _SearchBodyState();
+}
+
+class _SearchBodyState extends State<SearchBody> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
